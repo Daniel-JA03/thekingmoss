a13a2963260dae3655e4b41637390dab879b6e41
Daniel-JA03 <danieljaimesamancio@gmail.com>
2025-05-13 23:50:12 -0500

    feat: Agregue interfaz IDireccionRepository

---
+++ src/main/java/com/thekingmoss/domain/repository/IDireccionRepository.java
@@ -0,0 +1,10 @@
+package com.thekingmoss.domain.repository;
+
+import com.thekingmoss.domain.entity.Direccion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface IDireccionRepository extends JpaRepository<Direccion, Long> {
+    List<Direccion> findByDireccionByUsuario_IdUsuario(long idUsuario);
+}
