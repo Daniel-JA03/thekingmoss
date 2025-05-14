b9e91cb492ae8bb0b20e015072c89184f5472d62
Daniel-JA03 <danieljaimesamancio@gmail.com>
2025-05-13 23:46:08 -0500

    feat: Agregue interfaz IDocumentoIdentidad

---
+++ src/main/java/com/thekingmoss/domain/repository/IDocumentoIdentidad.java
@@ -0,0 +1,7 @@
+package com.thekingmoss.domain.repository;
+
+import com.thekingmoss.domain.entity.DocumentoIdentidad;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IDocumentoIdentidad extends JpaRepository<DocumentoIdentidad, Integer> {
+}
