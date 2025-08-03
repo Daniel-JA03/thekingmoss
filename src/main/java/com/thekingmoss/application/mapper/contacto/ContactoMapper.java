13a7d10a76e22827d26055b3f43372849a7d9854
Daniel-JA03 <danieljaimesamancio@gmail.com>
2025-08-02 23:45:42 -0500

    feat: se agrego mapper y repository para la entidad contacto

---
+++ src/main/java/com/thekingmoss/application/mapper/contacto/ContactoMapper.java
@@ -0,0 +1,29 @@
+package com.thekingmoss.application.mapper.contacto;
+
+import com.thekingmoss.application.dto.contacto.ContactoRequestDto;
+import com.thekingmoss.application.dto.contacto.ContactoResponseDto;
+import com.thekingmoss.domain.entity.Contacto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ContactoMapper {
+    public ContactoResponseDto toDto(Contacto contacto) {
+        return ContactoResponseDto.builder()
+                .contactoid(contacto.getContactoid())
+                .nombre(contacto.getNombre())
+                .email(contacto.getEmail())
+                .asunto(contacto.getAsunto())
+                .mensaje(contacto.getMensaje())
+                .fechaCreacion(contacto.getFechaCreacion())
+                .build();
+    }
+
+    public Contacto toEntity(ContactoRequestDto requestDto) {
+        return Contacto.builder()
+                .nombre(requestDto.getNombre())
+                .email(requestDto.getEmail())
+                .asunto(requestDto.getAsunto())
+                .mensaje(requestDto.getMensaje())
+                .build();
+    }
+}
