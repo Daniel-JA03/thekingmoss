09bed47d61b0e2f294fbd9f60c2a9cbea3d9d8ca
Daniel-JA03 <danieljaimesamancio@gmail.com>
2026-04-23 22:38:55 -0500

    feat(upload): integrate Cloudinary for image upload and deletion in UploadFileService

--- src/main/java/com/thekingmoss/application/service/UploadFileService.java
+++ src/main/java/com/thekingmoss/application/service/UploadFileService.java
@@ -1,5 +1,8 @@
 package com.thekingmoss.application.service;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -8,27 +11,38 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class UploadFileService {
-    private String folder = "imagesProducts" + File.separator;
+    private final Cloudinary cloudinary;
 
-    public String saveImage(MultipartFile file) throws IOException {
-        if (!file.isEmpty()) {
-            byte[] bytes = file.getBytes();
-            Path path = Paths.get(folder + file.getOriginalFilename());
-            Files.write(path, bytes);
-            return file.getOriginalFilename(); // Solo el nombre del archivo
+    public String saveImage(MultipartFile file)  {
+        try {
+            Map uploadResult = cloudinary.uploader().upload(
+                    file.getBytes(),
+                    ObjectUtils.emptyMap()
+            );
+
+            return uploadResult.get("secure_url").toString();
+
+        } catch (IOException e) {
+            throw new RuntimeException("Error al subir imagen", e);
         }
-        return "default.jpg";
     }
 
-    public void deleteImage(String nombre) {
-        Path path = Paths.get(folder + nombre);
+    public void deleteImage(String imagenUrl) {
         try {
-            Files.deleteIfExists(path);
-        } catch (IOException e) {
-            throw new RuntimeException("Error al eliminar la imagen", e);
+            // Extraer public_id desde la URL
+            String[] parts = imagenUrl.split("/");
+            String fileName = parts[parts.length - 1]; // musgo.webp
+            String publicId = fileName.substring(0, fileName.lastIndexOf('.')); // musgo
+
+            cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error al eliminar imagen", e);
         }
     }
 }
